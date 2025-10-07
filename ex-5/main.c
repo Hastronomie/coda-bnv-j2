6155e64cde505b5263a5901858df36dc788b2836
Mathilde <marco.mathilde@orange.fr>
2025-10-07 12:14:05 +0000

    Ajout exo 5

---
+++ ex-5/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	double nb1 = 12.0;
+	double nb2 = 7.0;
+	double resultat = (nb1 + nb2)/2;
+	printf("La moyenne de 12 et 7 est égale à %.2f\n", resultat);
+	exit(0);
+}
